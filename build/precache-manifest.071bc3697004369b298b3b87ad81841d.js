514940b85f1d11f26807fbc5178fb8278f290c68
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-02-10 22:54:30 +0100

    new build

---
+++ build/precache-manifest.071bc3697004369b298b3b87ad81841d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f4b975a0b2dcb943d3b",
+    "url": "/static/js/main.4f4b975a.chunk.js"
+  },
+  {
+    "revision": "f576244e398f67f7f98b",
+    "url": "/static/js/1.f576244e.chunk.js"
+  },
+  {
+    "revision": "4f4b975a0b2dcb943d3b",
+    "url": "/static/css/main.b98c99ef.chunk.css"
+  },
+  {
+    "revision": "81bc30a709e7548ebc2ae506371a7534",
+    "url": "/index.html"
+  }
+];
